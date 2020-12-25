85ff28648ace30a26f2f653059b23470a5549f00
Swung0x48 <swung0x48@outlook.com>
2020-12-25 11:34:43 +0800

    A sample controller using authentication

---
+++ src/main/java/com/swung0x48/librarymanager/controller/BookController.java
@@ -0,0 +1,18 @@
+package com.swung0x48.librarymanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    @GetMapping("")
+    public String getAllBooks(HttpServletRequest request) {
+        int userId = (Integer) request.getAttribute("userId");
+
+        return "userId: " + userId;
+    }
+}
