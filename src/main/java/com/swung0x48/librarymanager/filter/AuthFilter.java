651fd78af7f0d99be15b5f18402918148ce20ba7
Swung0x48 <swung0x48@outlook.com>
2020-12-25 11:31:47 +0800

    Add AuthFilter

---
+++ src/main/java/com/swung0x48/librarymanager/filter/AuthFilter.java
@@ -0,0 +1,55 @@
+package com.swung0x48.librarymanager.filter;
+
+import com.swung0x48.librarymanager.RuntimeConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.Jwts;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+        String authHeader = httpServletRequest.getHeader("Authorization");
+        if (authHeader != null) {
+            String[] authHeaderArray = authHeader.split("Bearer ");
+            if (authHeaderArray.length > 1 && authHeaderArray[1] != null) {
+                String token = authHeaderArray[1];
+
+                try {
+                    Claims claims = Jwts.parserBuilder()
+                            .setSigningKey(RuntimeConstants.API_SECRET_KEY)
+                            .build()
+                            .parseClaimsJws(token)
+                            .getBody();
+
+                    httpServletRequest.setAttribute("userId", Integer.parseInt(claims.get("userId").toString()));
+
+                } catch (Exception e) {
+                    httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid token");
+                    return;
+                }
+            } else {
+                httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Token ill-formed");
+                return;
+            }
+        } else {
+            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "no");
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
