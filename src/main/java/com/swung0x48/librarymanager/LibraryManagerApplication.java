84da0a5fdf7efeba637fa17f4096ec003c2c7aa9
Swung0x48 <swung0x48@outlook.com>
2020-12-25 11:34:27 +0800

    Inject AuthFilter to bean

--- src/main/java/com/swung0x48/librarymanager/LibraryManagerApplication.java
+++ src/main/java/com/swung0x48/librarymanager/LibraryManagerApplication.java
@@ -1,7 +1,10 @@
 package com.swung0x48.librarymanager;
 
+import com.swung0x48.librarymanager.filter.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryManagerApplication {
@@ -10,4 +13,13 @@ public class LibraryManagerApplication {
 		SpringApplication.run(LibraryManagerApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterFilterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+
+		registrationBean.addUrlPatterns("/api/books/*");
+		return registrationBean;
+	}
 }
